cec5d47013084838c6c9464f9811ded14a592cb3
Omar Al-Khashab <omar84@gmail.com>
2025-01-13 10:39:40 +0300

    store last used language in app settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -10,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Daliluk App',
+      title: 'Daliluk - دليلك',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -26,6 +29,7 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   late final WebViewController _controller;
+  String? _preferredLanguage;
 
   @override
   void initState() {
@@ -33,23 +37,67 @@ class _WebViewPageState extends State<WebViewPage> {
 
     // Initialize the WebViewController
     _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('https://dalil.uk?key=kit1')); // Replace with your website URL
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+    // Set the status bar color and icon brightness
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white, // Set the status bar color to white
+      statusBarIconBrightness: Brightness.dark, // Set the icon brightness to dark
+    ));
+
+    // Get the preferred language from SharedPreferences
+    _getPreferredLanguage().then((lang) {
+      _preferredLanguage = lang;
+
+      // Load the initial URL
+      _controller.loadRequest(Uri.parse('https://dalil.uk?key=kit1'));
+
+      // If preferred language is set, update the URL
+      if (_preferredLanguage != null) {
+        _updateUrlWithLanguage(_preferredLanguage!);
+      }
+    });
+
+    // Listen for URL changes using navigationDelegate
+    _controller.setNavigationDelegate(NavigationDelegate(
+      onPageStarted: (url) {
+        print('New URL: $url');
+
+        // Parse the URL and check for the 'lang' parameter
+        Uri uri = Uri.parse(url);
+        String? lang = uri.queryParameters['lang'];
+
+        if (lang != null && _preferredLanguage != lang) {
+          // Update the preferred language and URL
+          _preferredLanguage = lang;
+          _savePreferredLanguage(lang);
+        }
+      },
+    ));
+  }
+
+  // Update the WebView URL with the given language
+  Future<void> _updateUrlWithLanguage(String lang) async {
+    await _controller
+        .loadRequest(Uri.parse('https://dalil.uk?key=kit1&lang=$lang'));
+  }
+
+  // Save the preferred language to SharedPreferences
+  Future<void> _savePreferredLanguage(String lang) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('preferred_language', lang);
+    print('Preferred language saved: $lang');
+  }
+
+  // Get the stored preferred language
+  Future<String?> _getPreferredLanguage() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('preferred_language') ?? 'ar';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40), // Adjust height here
-        child: AppBar(
-          title: Text(
-            'Daliluk - دليلك',
-            style: TextStyle(fontSize: 16), // Smaller text size
-          ),
-          centerTitle: true, // Optional: Center the title
-        ),
-      ),
       body: SafeArea(
         child: WebViewWidget(controller: _controller),
       ),
